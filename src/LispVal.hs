4434fa846c002ac9839ed1772842f2a4c822a354
Jeff Cheah <goodboy96@live.com.my>
2020-08-09 22:56:01 +0800

    Parser done

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 
 module LispVal where
 
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Dynamic (Typeable)
-import qualified Data.Map as Map
-import qualified Data.Text as T
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Data.Dynamic         (Typeable)
+import qualified Data.Map             as Map
+import qualified Data.Text            as T
 
 data LispVal
   = Atom T.Text
@@ -29,22 +29,22 @@ type EnvCtx = Map.Map T.Text LispVal
 
 newtype Eval a = Eval {unEval :: ReaderT EnvCtx IO a}
   deriving
-    ( Monad,
-      Functor,
-      Applicative,
-      MonadReader EnvCtx,
-      MonadIO
+    ( Monad
+    , Functor
+    , Applicative
+    , MonadReader EnvCtx
+    , MonadIO
     )
 
 showVal :: LispVal -> T.Text
 showVal val =
   case val of
-    Atom atom -> atom
-    String str -> T.concat ["\"", str, "\""]
-    Number num -> T.pack $ show num
-    Bool True -> "#t"
-    Bool False -> "#f"
-    Nil -> "Nil"
+    Atom atom     -> atom
+    String str    -> T.concat ["\"", str, "\""]
+    Number num    -> T.pack $ show num
+    Bool True     -> "#t"
+    Bool False    -> "#f"
+    Nil           -> "Nil"
     List contents -> T.concat $ ["(", T.unwords $ showVal <$> contents, ")"]
-    Fun _ -> "(internal function)"
-    Lambda _ _ -> "(lambda function)"
+    Fun _         -> "(internal function)"
+    Lambda _ _    -> "(lambda function)"
